ec7e3ec62f9e8f58bcb0cb58ab80d8078a63cef0
aaaa <aaaa@aaa.com>
2024-04-04 09:51:41 +0800

    adding level data

--- learn_typing.py
+++ learn_typing.py
@@ -1,1 +1,9 @@
 import pygame
+
+class LearnTyping:
+	def __init__(self):
+		with open("./data/level_data.json") as f:
+			file = f.read()
+			self.level_data = eval(file)
+			print(self.level_data)
+l=LearnTyping()
