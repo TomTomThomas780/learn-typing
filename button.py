bdd6a679e1465ef8e755bff007780dffc9001609
aaaa <aaaa@aaa.com>
2024-04-05 12:55:56 +0800

    a almost full edition,no background music though.

--- button.py
+++ button.py
@@ -19,7 +19,7 @@ class Button:
         self.msg = msg
         
         # The button message needs to be prepped only once.
-        
+        self._prep_msg(self.msg)
 
     def _prep_msg(self, msg):
         """Turn msg into a rendered image and center text on the button."""
@@ -57,13 +57,78 @@ class Button2:
         self.msg = msg
         
         # The button message needs to be prepped only once.
-        
+        self._prep_msg(self.msg)
 
     def _prep_msg(self, msg):
         """Turn msg into a rendered image and center text on the button."""
         self.msg_image = self.font.render(msg, True, self.text_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Draw blank button and then draw message.
+        self._prep_msg(self.msg)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
+
+
+class Button3:
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont('SimSun', 20)
+        
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.left = self.screen_rect.left
+        self.msg = msg
+        
+        # The button message needs to be prepped only once.
+        self._prep_msg(self.msg)
+
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Draw blank button and then draw message.
+        self._prep_msg(self.msg)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
+
+class Button4:
+ 
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont('SimSun', 48)
+        
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.left = self.screen_rect.left
+        self.msg = msg
+        
+        # The button message needs to be prepped only once.
+        self._prep_msg(self.msg)
+
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.right = self.rect.right
+        self.msg_image_rect.bottom = self.rect.bottom
 
     def draw_button(self):
         # Draw blank button and then draw message.
--- learn_typing.py
+++ learn_typing.py
@@ -3,10 +3,12 @@ import button
 import sys
 import random
 import time
+from heart import Heart
+from scoreboard import Scoreboard as sb
 
 class LearnTyping:
 	def __init__(self):
-		with open("./data/level_data.json") as f:
+		with open("./data/level_data.json",encoding="utf-8") as f:
 			file = f.read()
 			self.level_data = eval(file)
 		pygame.init()
@@ -17,11 +19,23 @@ class LearnTyping:
 		self.screen_rect=self.screen.get_rect()
 		self.bg_color=(20,170,197)
 		self.quit_button=button.Button(self,"退出")
-		self.quit_button.rect.top=self.screen_rect.top
+		self.quit_button.rect.bottom=self.screen_rect.bottom
 		self.start_button=button.Button(self,"开始")
 		self.start_button.rect.center=self.screen_rect.center
-		self.game_mode='ready' #ready,select,play
+		self.game_mode='ready' #ready,select,pre-play,play
 		self.right=[]
+		self.health=5
+		self.board=sb(self)
+		self.sheet_of_typing=pygame.image.load("images/sheet_of_typing.bmp")
+		self.rect_of_SFT=self.sheet_of_typing.get_rect()
+		self.rect_of_SFT.center=self.screen_rect.center
+		self.rect_of_SFT.top=self.screen_rect.top
+		self.go_back_button=button.Button4(self,'返回')
+		self.go_back_button.rect.top=self.screen_rect.top
+		self.go_back_button.rect.right=self.screen_rect.right
+		self.SFT_button=button.Button4(self,'指法表')
+		self.SFT_button.rect.top=self.screen_rect.top
+		self.SFT_button.rect.right=self.screen_rect.right
 		try:
 			with open('data/level.json') as f:
 				self.level_now=int(f.read())
@@ -33,9 +47,14 @@ class LearnTyping:
 		level_name=f"level_{level}"
 		data=self.level_data[level_name]
 		if data['type'] == 'learn':
+			data1=[]
+			for i in range(1,level+1):
+				data1.extend(self.level_data[f"level_{i}"]['new_key'])
 			k=""
 			for i in range(50):
 				k+=random.choice(data['new_key'])
+			for i in range(20):
+				k+=random.choice(data1)
 			return k
 		elif data['type'] == 'review':
 			data1=[]
@@ -65,6 +84,7 @@ class LearnTyping:
 				mouse_pos=pygame.mouse.get_pos()
 				self._check_quit_button(mouse_pos)
 				self._check_start_button(mouse_pos)
+				self._check_go_back_or_SFT_button(mouse_pos)
 			if event.type==pygame.KEYDOWN:
 				if event.key==pygame.K_ESCAPE:
 					with open('data/level.json','w') as f:
@@ -102,10 +122,48 @@ class LearnTyping:
 							self.str=self.generate_level_string(int(self.answer))
 							self.all=list(self.str)[:10]
 							self.type=self.level_data[f"level_{self.answer}"]['type']
+							stay_in=True
+							while stay_in:
+								for event in pygame.event.get():
+									if event.type==pygame.QUIT:
+										with open('data/level.json','w') as f:
+											f.write(f'{self.level_now}')
+											sys.exit()
+									elif event.type==pygame.MOUSEBUTTONDOWN:
+										mouse_pos=pygame.mouse.get_pos()
+										self._check_quit_button(mouse_pos)
+										self._check_go_back_or_SFT_button(mouse_pos)
+										stay_in=False
+									if event.type==pygame.KEYDOWN:
+										if event.key==pygame.K_ESCAPE:
+											with open('data/level.json','w') as f:
+												f.write(f'{self.level_now}')
+												sys.exit()
+
+								self.screen.fill(self.bg_color)
+								self.quit_button.draw_button()
+								self.go_back_button.draw_button()
+								sth=self.level_data[f'level_{self.answer}']
+								if self.type=='learn':
+									string_show=f"这关要学习的是{sth['new_key'][0]},{sth['new_key'][1]},分别用{sth['msg'][0][0]}{sth['msg'][0][1]}{sth['msg'][0][2]}和{sth['msg'][1][0]}{sth['msg'][1][1]}{sth['msg'][1][2]}来打。"
+									button_show=button.Button2(self,string_show)
+									button_show.rect.center=self.screen_rect.center
+								else:
+									string_show=f"这关是{sth['msg'][0]}。"
+									button_show=button.Button2(self,string_show)
+									button_show.rect.center=self.screen_rect.center
+								button_show.draw_button()
+								button_tip=button.Button3(self,'(鼠标单击继续)')
+								button_tip.rect.center=self.screen_rect.center
+								button_tip.rect.top=button_show.rect.bottom
+								button_tip.draw_button()
+								self.screen.blit(self.sheet_of_typing,self.rect_of_SFT)
+								pygame.display.flip()
 							if self.level_data[f"level_{self.answer}"]['type'] != 'endless':
 								self.time=self.level_data[f"level_{self.answer}"]['time']
 							else:
 								self.health=self.level_data[f"level_{self.answer}"]['health']
+							self.time_typed=time.time()
 							self.start_time=time.time()
 						else:
 							self.screen.fill(self.bg_color)
@@ -117,6 +175,8 @@ class LearnTyping:
 							time.sleep(3)
 							self.game_mode='select'
 				elif self.game_mode=='play':
+					if self.type=='endless':
+						self.time_typed=time.time()
 					try:
 						if chr(int(str(event.key)))==self.str[0]:
 							self.str=self.str[1:]
@@ -131,10 +191,11 @@ class LearnTyping:
 								self.plus_time+=1
 							else:
 								self.health-=1
+								self.board.prep_hearts()
 								if self.health<=0:
 									self.game_mode='ready'
 									self.end_time=time.time()
-									c=button.Button2(self,f'你打了{self.typed}个字,花了{round(self.end_time-self.start_time)}秒。')
+									c=button.Button2(self,f'你打对了{self.typed}个字,花了{round(self.end_time-self.start_time)}秒。')
 									self.screen.fill(self.bg_color)
 									b.rect.center=self.screen_rect.center
 									b.draw_button()
@@ -144,10 +205,10 @@ class LearnTyping:
 						if self.str=='' and self.type!='endless':
 							self.end_time=time.time()
 							self.game_mode='ready'
-							if int(self.answer)==self.level_now and self.level_now!=20:
-								self.level_now+=1
 							if round(self.end_time-self.start_time+self.plus_time)<=self.time:
 								b=button.Button2(self,f'恭喜你！你用了{round(self.end_time-self.start_time)}+{self.plus_time}(打错惩罚)秒通过了这关。')
+								if int(self.answer)==self.level_now and self.type!='endless':
+									self.level_now+=1
 							else:
 								b=button.Button2(self,f'你用了{round(self.end_time-self.start_time)}+{self.plus_time}(打错惩罚)秒完成了这关,但超过了{self.time}秒。')
 							self.screen.fill(self.bg_color)
@@ -164,10 +225,11 @@ class LearnTyping:
 								self.plus_time+=1
 							else:
 								self.health-=1
+								self.board.prep_hearts()
 								if self.health<=0:
 									self.game_mode='ready'
 									self.end_time=time.time()
-									c=button.Button2(self,f'你打了{self.typed}个字,花了{round(self.end_time-self.start_time)}秒。')
+									c=button.Button2(self,f'你打对了{self.typed}个字,花了{round(self.end_time-self.start_time)}秒。')
 									self.screen.fill(self.bg_color)
 									c.rect.center=self.screen_rect.center
 									c.draw_button()
@@ -184,6 +246,7 @@ class LearnTyping:
 
 	def _check_start_button(self,mouse_pos):
 		if self.start_button.rect.collidepoint(mouse_pos) and self.game_mode=='ready':
+			self.time_typed=time.time()
 			self.typed=0
 			self.type=''
 			self.time=0
@@ -195,6 +258,35 @@ class LearnTyping:
 			self.select_button=button.Button(self,f"请输入关卡编号(1~{self.level_now})")
 			self.select_button.rect.center=self.screen_rect.center
 			self.select_button.rect.left=self.screen_rect.left
+
+	def _check_go_back_or_SFT_button(self,mouse_pos):
+		if self.go_back_button.rect.collidepoint(mouse_pos) and self.game_mode!='ready':
+			if self.game_mode=='select':
+				self.game_mode='ready'
+			elif self.game_mode=='play':
+				self.game_mode='select'
+		elif self.SFT_button.rect.collidepoint(mouse_pos) and self.game_mode=='ready':
+			stay_in=True
+			while stay_in:
+				for event in pygame.event.get():
+					if event.type==pygame.QUIT:
+						with open('data/level.json','w') as f:
+							f.write(f'{self.level_now}')
+							sys.exit()
+					elif event.type==pygame.MOUSEBUTTONDOWN:
+						mouse_pos=pygame.mouse.get_pos()
+						self._check_quit_button(mouse_pos)
+						stay_in=False
+				self.screen.fill(self.bg_color)
+				self.quit_button.draw_button()
+				sth=button.Button3(self,'（鼠标单击继续）')
+				sth.rect.center=self.screen_rect.center
+				sth.draw_button()
+				self.screen.blit(self.sheet_of_typing,self.rect_of_SFT)
+				pygame.display.flip()
+
+
+
 
 	def _update_screen(self):
 		self.screen.fill(self.bg_color)
@@ -216,7 +308,33 @@ class LearnTyping:
 			self.playing_button1.rect.center=self.screen_rect.center
 			self.playing_button1.rect.left=self.playing_button.msg_image_rect.right
 			self.playing_button1.draw_button()
-
+			if self.type=='endless':
+				if (time.time()-self.time_typed)>=3:
+					self.health-=1
+					self.board.prep_hearts()
+					self.time_typed=time.time()
+					if self.health<=0:
+						self.game_mode='ready'
+						self.end_time=time.time()
+						c=button.Button2(self,f'你打对了{self.typed}个字,花了{round(self.end_time-self.start_time)}秒。')
+						self.screen.fill(self.bg_color)
+						c.rect.center=self.screen_rect.center
+						c.draw_button()
+						pygame.display.flip()
+						time.sleep(3)
+				self.board.show_score()
+		if self.game_mode!='ready':
+			self.go_back_button.draw_button()
+		else:
+			self.SFT_button.draw_button()
+		if self.game_mode=='play':
+			if self.type!='endless':
+				show_the_button=button.Button2(self,f'{round(time.time()-self.start_time)}+{self.plus_time}秒')
+			else:
+				show_the_button=button.Button2(self,f'请在{round(3-(time.time()-self.time_typed))}秒内打字。你已用{round(time.time()-self.start_time)}秒。')				
+			show_the_button.rect.center=self.screen_rect.center
+			show_the_button.rect.top=self.screen_rect.top
+			show_the_button.draw_button()
 		pygame.display.flip()
 
 	def run_game(self):
